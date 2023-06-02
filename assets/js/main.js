b3b6c09199bddc0943a3f6469e042e0638283a12
yasna angulo <yasna.angulo>
2023-06-02 00:44:44 -0400

    subiendo primera parte del main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
+
+
+
 $(document).ready(function() {
+    $("#formulario_de_registro_ppto").submit(function(evento){
+        evento.preventDefault()
+        let presupuesto=$("#nameExpense").val()
+        alert(presupuesto)
+    })
+
+
+    
 const presupuesto=$("#nameExpense")
 
 $(document).on("input","#nameExpense",function(){
