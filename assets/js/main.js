881b018e11dfa1ac231b9e8a1f70eaa98c4b8df9
yasna angulo <yasna.angulo>
2023-06-02 22:10:32 -0400

    subiendo main.js para que aparezca en el proyecto

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,4 @@ $(document).ready(function() {
     })
 
 
-    
-const presupuesto=$("#nameExpense")
 
-$(document).on("input","#nameExpense",function(){
-console.log(presupuesto.val())
-
-})
-
-
-})
