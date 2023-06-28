e368d76557d2c4cf358a895baa4bcad5aad254b3
yasna angulo <yasna.angulo>
2023-06-27 20:31:45 -0400

    subiendo actualización del main donde se pudo agregar presupuesto. Falta aun que calculen los demás botones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,57 @@
 $(document).ready(function () {
-    $("#formulario_de_registro_ppto").submit(function (evento) {
+    let listaGastos = []
+
+    $("#formularioRegistro").submit(function (evento) {
         evento.preventDefault()
-        let presupuesto = $("#nameExpense").val()
-        alert(presupuesto)
+        let presupuesto = $("#ingresoMontoPresupuesto").val()
+
+        $("#mostrarPresupuesto").attr("data-value", presupuesto)
+
+        $("#mostrarPresupuesto").html("$" + presupuesto)
+
+        $("#ingresoMontoPresupuesto").val("")
     })
+
+    $("#botonGasto").click(() => {
+
+
+        let nombreGasto = $("#ingresoNombreGasto").val().trim()
+        let montoGasto = parseInt($("#ingresoMontoGasto").val())
+        listaGastos.push({nombre:nombreGasto, monto:montoGasto})
+
+        dibujarLista()
+
+
+    })
+
+    function dibujarLista(){
+
+
+$("#mostrarListaGastos").html("");
+
+    listaGastos.forEach((gasto, index) => {
+
+      $("#mostrarListaGastos").append(
+
+        `<tr>
+
+          <td>${gasto.nombre}</td>
+
+          <td>$${gasto.monto.toLocaleString("es-CL")}</td>
+
+          <td>
+
+            <i class="fa-solid fa-trash-can btn btn-primary" data-index="${index}"></i>
+
+          </td>
+
+        </tr>`
+
+      );
+
+    });
+    }
+//TODO falta implementar validaciones y calcular saldo y limpiar formularios
 
 })
 
